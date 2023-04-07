e4859e8145986c1c867d2285276e1914243aa9b0
Camilo Abarca <cabarcaz@yahoo.cl>
2023-04-07 01:34:17 -0400

    Inicio proyecto

---
+++ ui-twitter/src/App.jsx
@@ -0,0 +1,48 @@
+import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard';
+
+
+const users = [
+  {
+    userName: 'midudev',
+    name: 'Miguel Ángel Durán',
+    isFollowing: true
+  },
+  {
+    userName: 'pheralb',
+    name: 'Pablo H.',
+    isFollowing: false
+  },
+  {
+    userName: 'PacoHdez',
+    name: 'Paco Hdez',
+    isFollowing: true
+  },
+  {
+    userName: 'TMChein',
+    name: 'Tomas',
+    isFollowing: false
+  }
+]
+
+export function App () {
+
+  return (
+    <section className='app'>
+    {
+      users.map(user => {
+        const { userName, name, isFollowing } = user
+        return(
+          <TwitterFollowCard
+          userName={userName}
+          initialIsFollowing={isFollowing}
+          key={userName}
+          >
+          {name}
+          </TwitterFollowCard>
+        )
+      })
+    }
+    </section>
+  );
+}
